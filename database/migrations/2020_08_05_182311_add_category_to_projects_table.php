57756ebe0b243f4fba7648aa997853b20b809076
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-08-05 22:42:33 -0500

    se agregaron las cetogorias al proyecto

---
+++ database/migrations/2020_08_05_182311_add_category_to_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryToProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('projects_category_id_foreign');
+            $table->dropColumn('category_id');
+        });
+    }
+}
