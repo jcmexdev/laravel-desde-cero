52692bfec37864bc56739b786bc74b4f84d80baa
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-08-06 19:23:57 -0500

    filtrado de proyectos por categoría

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,7 +5,15 @@
 @section('content')
 <div class="container">
     <div class="d-flex justify-content-between align-items-center mb-3">
-        <h1 class="display-4 mb-0">@lang('Projects')</h1>
+        @isset($category)
+            <div>
+                <h1 class="display-4 mb-0">{{ $category->name }}</h1>
+                <a href="{{ route('projects.index') }}">Regresar al portafolio</a>
+            </div>
+        @else
+            <h1 class="display-4 mb-0">@lang('Projects')</h1>
+        @endisset
+
         @auth
         <a class="btn btn-primary" href="{{ route('projects.create') }}">Crear proyecto</a>
         @endauth
@@ -13,7 +21,7 @@
     <p class="lead text-secondary">Proyectos realizados Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
     <div class="d-flex flex-wrap justify-content-between align-items-start">
         @forelse($projects as $project)
-        <div class="card mb-2" style="width: 18rem;">
+        <div class="card mb-4 shadow-sm" style="width: 18rem;">
             @if($project->image)
             <img class="card-img-top" src="{{ asset('storage/'.$project->image) }}" alt="{{ $project->title }}"
                 style="height:150px; object-fit:cover;">
@@ -26,7 +34,9 @@
                 <p class="card-text text-truncate">{{ $project->description }}</p>
                 <div class="d-flex justify-content-between align-items-center">
                     <a class="btn btn-primary btn-sm" href="{{ route('projects.show', $project) }}">Ver más...</a>
-                    <a href="#" class="badge badge-secondary">{{ optional($project->category)->name }}</a>
+                    @if ($project->category_id)
+                        <a href="{{ route('categories.show', $project->category) }}" class="badge badge-secondary">{{ $project->category->name }}</a>
+                    @endif
                 </div>
             </div>
         </div>
@@ -35,7 +45,7 @@
             No hay proyectos para mostrar
         </li>
         @endforelse
-        {{ $projects->links() }}
     </div>
+    {{ $projects->links() }}
 </div>
 @endsection
